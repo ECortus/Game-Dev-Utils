5acb66c72e5e7025dde7faf797512dddfd7df278
ECortus <ve.cortus@gmail.com>
2026-01-27 02:01:20 +0100

    add actor

---
+++ Assets/Plugins/Tools/GameDevUtils/Runtime/Simultaneous/ActorDescription.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace GameDevUtils.Runtime.Simultaneous
+{
+    public interface IActorInformation
+    {
+        string Id { get; }
+        
+        string Name { get; }
+        string Description { get; }
+        
+        Sprite Icon { get; }
+    }
+    
+    [Serializable]
+    public sealed class ActorInformation : IActorInformation
+    {
+        [SerializeField] private string id = "id_default_actor";
+
+        [Space(5)] 
+        [SerializeField] private string name = "Default Actor Name";
+        [TextArea] [SerializeField] private string description = "Default actor description about specific info.";
+
+        [Space(5)] 
+        [SerializeField] private Sprite icon = null;
+
+        #region Interface
+
+        public string Id => id;
+        public string Name => name;
+        public string Description => description;
+        public Sprite Icon => icon;
+
+        #endregion
+    }
+}
--- Assets/Plugins/Tools/GameDevUtils/Runtime/UI/Abstract/Base/UDynamicField.cs
+++ Assets/Plugins/Tools/GameDevUtils/Runtime/UI/Abstract/Base/UDynamicField.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Cysharp.Threading.Tasks;
+using GameDevUtils.Runtime.Simultaneous;
 using UnityEngine;
 
 namespace GameDevUtils.Runtime.UI.Abstract
