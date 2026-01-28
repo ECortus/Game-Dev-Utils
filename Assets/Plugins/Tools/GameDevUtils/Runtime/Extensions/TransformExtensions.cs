4ad893c9150913be819fa47a8d64dca2b561b339
ECortus <ve.cortus@gmail.com>
2026-01-29 00:21:35 +0100

    upd

---
+++ Assets/Plugins/Tools/GameDevUtils/Runtime/Extensions/TransformExtensions.cs
@@ -0,0 +1,18 @@
+﻿using GameDevUtils.Runtime;
+using UnityEngine;
+
+namespace Plugins.Tools.GameDevUtils.Runtime.Extensions
+{
+    public static class TransformExtensions
+    {
+        public static void DestroyAllChildren(this Transform transform)
+        {
+            foreach (Transform child in transform)
+            {
+                ObjectHelper.Destroy(child.gameObject, true);
+            }
+            
+            transform.DetachChildren();
+        }
+    }
+}
